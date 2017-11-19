7acc0160423910826bba6bccba453fd34a86c0cf
rossfrank <rossfrank27@gmail.com>
2017-11-18 20:20:41 -0500

    update prod config

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,8 @@ use Mix.Config
 # which you typically run after static files are built.
 config :stock_game, StockGameWeb.Endpoint,
   load_from_system_env: true,
-  url: [host: "example.com", port: 80],
+  url: [host: "stocks.rossfrank.party", port: 8000],
+  server: true,
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
